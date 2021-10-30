b362eae9a285613974c7466d500c06b8b0b9b9b9
agussur685 <61925042+agussur27@users.noreply.github.com>
2021-10-30 23:35:48 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("program menghitung Luas dan Keliling Lingkaran")
+r = int(input("masukkan Jari -jari =" ))
+
+phi = 22/7
+luas = phi*r*r
+kel = 2*phi*r
+
+print("Luas Lingkaran Adalah       =", luas)
+print("Keliling Lingkaran Adalah   =",kel)
+
